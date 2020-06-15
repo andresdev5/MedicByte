d7e5dc694714c3948e9a3dde0cace2f0f8d14d73
Michael Cobacango <michaelpm63@gmail.com>
2020-06-15 18:42:37 -0500

    Create Class User, UserInfo, and Gender

---
+++ 06-Code/POO6382MedicByte/src/ec/edu/espe/medicbyte/model/User.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.medicbyte.model;
+
+/**
+ *
+ * @author Michael Cobacango -ESPE -DCCO
+ */
+public class User {
+    private String name;
+    private UserInfo info;
+    private Role role;
+    private String identificationCard;
+    
+    public boolean hasRole(){
+        //TODO
+        return false;
+    }
+    //Method sendEmail
+    
+          
+}
---
+++ 06-Code/POO6382MedicByte/src/ec/edu/espe/medicbyte/model/UserInfo.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.medicbyte.model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Michael Cobacango -ESPE -DCCO
+ */
+public class UserInfo {
+
+    private String firstName;
+    private String lastName;
+    private Date birthday;
+    private Gender gender;
+    private String email;
+    private String phone;
+    private String address;
+
+}
