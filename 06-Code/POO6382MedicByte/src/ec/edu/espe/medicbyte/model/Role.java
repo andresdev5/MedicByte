60c54222a0d50b4b730004ea61d333a80ce8675a
JuniorStalinJP <s_stalin999@hotmail.com>
2020-06-15 19:26:07 -0500

    MedicByte v1.0.0

---
+++ 06-Code/POO6382MedicByte/src/ec/edu/espe/medicbyte/model/Role.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.medicbyte.model;
+
+/**
+ *
+ * @author Junior Stalin Jurado Peña - ESPE - DCCO
+ */
+public class Role {
+    private String name;
+    private String displayName;
+    private Perimission Type[];
+    
+    
+    public void addPermission(Perimission  permission){
+    
+    }
+    
+    public void removePermission(int id){
+    
+    }
+    
+}
