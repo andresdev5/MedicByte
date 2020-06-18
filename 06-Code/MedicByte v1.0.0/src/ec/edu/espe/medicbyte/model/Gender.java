d40655dbabf334537c31fd05eab3e3b140dc8160
JuniorStalinJP <s_stalin999@hotmail.com>
2020-06-17 21:35:16 -0500

    MedicByte v 1.0.0

---
+++ 06-Code/MedicByte v1.0.0/src/ec/edu/espe/medicbyte/model/Gender.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.medicbyte.model;
+
+/**
+ *
+ * @author Junior Stalin Jurado Peña - ESPE - DCCO
+ */
+public class Gender {
+    
+}
--- 06-Code/MedicByte v1.0.0/src/ec/edu/espe/medicbyte/model/User.java
+++ 06-Code/MedicByte v1.0.0/src/ec/edu/espe/medicbyte/model/User.java
@@ -11,4 +11,65 @@ package ec.edu.espe.medicbyte.model;
  */
 public class User {
     
+   private String identificationcard;
+   private String surname;
+   private String name; 
+   private String phone;
+   private String email;
+   private Gender gender;
+   
+   
+
+    public String getIdentificationcard() {
+        return identificationcard;
+    }
+
+    public void setIdentificationcard(String identificationcard) {
+        this.identificationcard = identificationcard;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+   
+   
+   
+    
+    
 }
