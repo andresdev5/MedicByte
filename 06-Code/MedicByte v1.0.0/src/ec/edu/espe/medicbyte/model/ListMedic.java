0af075ce890ea5c5b157f32118eede7b827624a5
Michael Cobacango <michaelpm63@gmail.com>
2020-06-21 11:17:08 -0500

    MedicByte v1.0.0

--- 06-Code/MedicByte v1.0.0/src/ec/edu/espe/medicbyte/model/ListMedic.java
+++ 06-Code/MedicByte v1.0.0/src/ec/edu/espe/medicbyte/model/ListMedic.java
@@ -17,20 +17,20 @@ public class ListMedic {
     Collection<Medic> listMedic = new ArrayList();
     Collection<Medic> listMedicPediatric = new ArrayList();
     Collection<Medic> listMedicOdontology = new ArrayList();
-    
+    Collection<Medic> listMedicTraumatology = new ArrayList();
 
     Medic medic = new Medic("1722066956", "Michael", "Cobacango",
-            "Odontologo", "0999082067", "michaelpm63@gmail.com", Gender.MALE);
+            "Odontology", "0999082067", "michaelpm63@gmail.com", Gender.MALE);
     Medic medic1 = new Medic("1710379346", "Alfonso", "Cadena",
-            "Cirujano", "0985623744", "alfonso@gmail.com", Gender.MALE);
+            "Odontology", "0985623744", "alfonso@gmail.com", Gender.MALE);
     Medic medic2 = new Medic("1714548846", "Juan", "Pardo",
             "Pediatric", "0958421365", "juanpa@gmail.com", Gender.MALE);
     Medic medic3 = new Medic("1711558915", "Maria", "Salazar",
             "Pediatric", "0985474547", "mariasa@gmail.com", Gender.FEMALE);
     Medic medic4 = new Medic("1723658945", "Paola", "Suarez",
-            "Pediatric", "0936541225", "paolasu@gmail.com", Gender.FEMALE);
+            "Traumatology", "0936541225", "paolasu@gmail.com", Gender.FEMALE);
     Medic medic5 = new Medic("1752684123", "Damaris", "Cartagena",
-            "Odontologa", "0947521635", "damaris@gmail.com", Gender.FEMALE);
+            "Traumatology", "0947521635", "damaris@gmail.com", Gender.FEMALE);
 
     public void enterMedic() {
         listMedic.add(medic);
@@ -59,7 +59,17 @@ public class ListMedic {
     public void saveMedicOdontology(){
         for (Medic medicsOdontology: listMedic){
             if(medicsOdontology.getSpeciality()=="Odontology"){
-                
+                listMedicOdontology.add(medicsOdontology);
+                System.out.println(medicsOdontology);
+            }
+        }
+    }
+    
+    public void saveMedicTraumatology(){
+        for (Medic medicsTraumatology : listMedic){
+            if(medicsTraumatology.getSpeciality()=="Traumatology"){
+                listMedicTraumatology.add(medicsTraumatology);
+                System.out.println(medicsTraumatology);
             }
         }
     }
