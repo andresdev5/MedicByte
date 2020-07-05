f628804832103f201a1f3963b1649febcdeb3646
Michael Cobacango <michaelpm63@gmail.com>
2020-07-04 23:31:47 -0500

    Added MedicsController

---
+++ 06-Code/src/main/java/ec/edu/espe/medicbyte/controller/MedicsController.java
@@ -0,0 +1,14 @@
+package ec.edu.espe.medicbyte.controller;
+
+import ec.edu.espe.medicbyte.common.core.Controller;
+
+/**
+ *
+ * @author Michael Cobacango
+ */
+public class MedicsController extends Controller {
+    @Override
+    public void init() {
+        // TODO: setup view and logic
+    }
+}
