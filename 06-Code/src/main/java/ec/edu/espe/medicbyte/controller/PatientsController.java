9eeff3a5ca3560370dd7cddabfd61f09cb3f6004
JuniorStalinJP <s_stalin999@hotmail.com>
2020-07-05 23:28:02 -0500

    Create MedicsController

---
+++ 06-Code/src/main/java/ec/edu/espe/medicbyte/controller/PatientsController.java
@@ -0,0 +1,14 @@
+package ec.edu.espe.medicbyte.controller;
+
+import ec.edu.espe.medicbyte.common.core.Controller;
+
+/**
+ *
+ * @author Junior Jurado
+ */
+public class PatientsController extends Controller {
+    @Override
+    public void init() {
+        // ...
+    }
+}
