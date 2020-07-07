3407b274001fca54ee060c09d508c61f0fecf462
Johnny <jsloachamin1@espe.edu.ec>
2020-07-06 23:48:03 -0500

    Added accounts controller

---
+++ 06-Code/src/main/java/ec/edu/espe/medicbyte/controller/AccountController.java
@@ -0,0 +1,14 @@
+package ec.edu.espe.medicbyte.controller;
+
+import ec.edu.espe.medicbyte.common.core.Controller;
+
+/**
+ *
+ * @author Johnny Loachamin
+ */
+public class AccountController extends Controller {
+    @Override
+    public void init() {
+        // ...
+    }
+}
