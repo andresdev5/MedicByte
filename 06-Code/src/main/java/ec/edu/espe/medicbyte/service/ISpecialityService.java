fc74d08f64556486e1dfce5931e707851be89f1a
Johnny <jsloachamin1@espe.edu.ec>
2020-07-26 08:39:19 -0500

    Add SpecialityService

---
+++ 06-Code/src/main/java/ec/edu/espe/medicbyte/service/ISpecialityService.java
@@ -0,0 +1,16 @@
+package ec.edu.espe.medicbyte.service;
+
+import ec.edu.espe.medicbyte.model.Speciality;
+import java.util.List;
+
+/**
+ *
+ * @author Johnny Loachamin
+ */
+public interface ISpecialityService {
+    public List<Speciality> getAllSpecialities();
+    public int getTotalSpecialities();
+    public boolean addSpeciality(Speciality speciality);
+    public Speciality getSpeciality(int id);
+    public Speciality getSpeciality(String name);
+}
