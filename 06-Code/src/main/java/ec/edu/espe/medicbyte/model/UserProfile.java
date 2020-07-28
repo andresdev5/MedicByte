326c413c2ba48c108e86e13fcd3ee41d1628553a
whitedev93 <ajjacome2@espe.edu.ec>
2020-07-27 20:06:01 -0500

    added UserProfile Model

---
+++ 06-Code/src/main/java/ec/edu/espe/medicbyte/model/UserProfile.java
@@ -0,0 +1,57 @@
+package ec.edu.espe.medicbyte.model;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author Andres Jonathan J.
+ */
+public class UserProfile {
+    public static enum Gender { MALE, FEMALE, UNSPECIFIED }
+    
+    private int userId;
+    private String fullName;
+    private String phone;
+    private Gender gender;
+    private LocalDate birthday;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+    
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+}
